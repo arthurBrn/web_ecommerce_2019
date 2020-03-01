5d27d5ba3679af6bbba9f626eceacbf6c2b53f8c
arthur baron <baronarthur3@gmail.com>
2020-03-01 17:42:00 +0100

    [EDIT] code clean. Entity creation ongoign.

--- backend/src/Entity/Product.php
+++ backend/src/Entity/Product.php
@@ -18,8 +18,59 @@ class Product
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $listing_picture;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getListingPicture(): ?string
+    {
+        return $this->listing_picture;
+    }
+
+    public function setListingPicture(?string $listing_picture): self
+    {
+        $this->listing_picture = $listing_picture;
+
+        return $this;
     }
 }
