d6834b976ca3a4be49ed5fae1ff62bc7edb0ab53
arthur baron <baronarthur3@gmail.com>
2020-03-01 23:03:21 +0100

    [EDIT] trying to add volume to mariadb container.

--- backend/src/Migrations/Version20200301172302.php
+++ backend/src/Migrations/Version20200301172302.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200301163654 extends AbstractMigration
+final class Version20200301172302 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -21,8 +21,9 @@ final class Version20200301163654 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, name LONGTEXT DEFAULT NULL, description LONGTEXT DEFAULT NULL, listing_picture LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-       }
+
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, name LONGTEXT DEFAULT NULL, description LONGTEXT DEFAULT NULL, listingPicture LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
 
     public function down(Schema $schema) : void
     {
