5b91f766c4a27ff38449a8d4ae4f3695ec5dab92
arthur baron <baronarthur3@gmail.com>
2020-03-13 15:04:32 +0100

    [EDIT] front component created.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
+import { HeadComponent } from './head/head.component';
+import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeadComponent,
+    BodyComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
