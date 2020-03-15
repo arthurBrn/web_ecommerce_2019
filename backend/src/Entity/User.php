a26c192936e72b2a38b524a316c2b9f75d3653f2
arthur baron <baronarthur3@gmail.com>
2020-03-15 15:31:41 +0100

    [EDIT] User entity operations done.

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -2,10 +2,18 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
+ * @ApiResource(
+ *     attributes={
+ *          "normalization_context"={"groups"={"user.read"}},
+ *          "denormalization_context"={"groups"={"user.write"}}
+ *     }
+ * )
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User implements UserInterface
@@ -14,52 +22,62 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"user.read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"user.read"})
      */
     private $email;
 
     /**
      * @ORM\Column(type="json")
+     * @Groups({"user.read"})
      */
     private $roles = [];
 
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Groups({"user.read"})
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"user.read"})
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"user.read"})
      */
     private $lastName;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"user.read"})
      */
     private $country;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"user.read"})
      */
     private $billingAddress;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"user.read"})
      */
     private $deliveryAddress;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Order", inversedBy="users")
+     * @Groups({"user.read"})
      */
     private $orders;
 
